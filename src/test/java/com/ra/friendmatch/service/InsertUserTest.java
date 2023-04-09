8e940145828abb8f081c8eb32b40b8cbe494eaaf
jieniyu <jieni.yu42@gmail.com>
2023-04-10 00:28:47 +0200

    add assets

--- src/test/java/com/ra/friendmatch/service/InsertUserTest.java
+++ src/test/java/com/ra/friendmatch/service/InsertUserTest.java
@@ -25,72 +25,72 @@ public class InsertUserTest {
      * 批量插入用户
      */
 
-    @Test
-    public void doInsertUser() {
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        final int INSERT_NUM = 1000;
-        List<User> userList = new ArrayList<>();
-        for (int i = 0; i < INSERT_NUM; i++) {
-            User user = new User();
-            user.setUsername("fake");
-            user.setUserAccount("fakerara");
-            user.setProfile("fake");
-            user.setAvatarUrl("");
-            user.setGender(0);
-            user.setUserPassword("12345678");
-            user.setPhone("123");
-            user.setEmail("123@gmail.com");
-            user.setUserStatus(0);
-            user.setUserRole(0);
-            user.setUserCode("111111");
-            user.setTags("[]");
-            userList.add(user);
-        }
-        userService.saveBatch(userList, 1000);
-        stopWatch.stop();
-        System.out.println(stopWatch.getTotalTimeMillis());
-    }
-
-    @Test
-    public void doConcurrencyInsertUser() {
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        final int INSERT_NUM = 10000;
-
-        int j = 0;
-        int batchSize = 5000;
-        List<CompletableFuture<Void>> futureList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            List<User> userList = new ArrayList<>();
-            while (true) {
-                j++;
-                User user = new User();
-                user.setUsername("fake");
-                user.setUserAccount("fakerara");
-                user.setProfile("fake");
-                user.setAvatarUrl("");
-                user.setGender(0);
-                user.setUserPassword("12345678");
-                user.setPhone("123");
-                user.setEmail("123@gmail.com");
-                user.setUserStatus(0);
-                user.setUserRole(0);
-                user.setUserCode("111111");
-                user.setTags("[]");
-                userList.add(user);
-                if (j % batchSize == 0) {
-                    break;
-                }
-            }
-            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-                userService.saveBatch(userList, batchSize);
-            }, executorService);
-            futureList.add(future);
-        }
-        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[]{})).join();
-
-        stopWatch.stop();
-        System.out.println(stopWatch.getTotalTimeMillis());
-    }
+//    @Test
+//    public void doInsertUser() {
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//        final int INSERT_NUM = 1000;
+//        List<User> userList = new ArrayList<>();
+//        for (int i = 0; i < INSERT_NUM; i++) {
+//            User user = new User();
+//            user.setUsername("fake");
+//            user.setUserAccount("fakerara");
+//            user.setProfile("fake");
+//            user.setAvatarUrl("");
+//            user.setGender(0);
+//            user.setUserPassword("12345678");
+//            user.setPhone("123");
+//            user.setEmail("123@gmail.com");
+//            user.setUserStatus(0);
+//            user.setUserRole(0);
+//            user.setUserCode("111111");
+//            user.setTags("[]");
+//            userList.add(user);
+//        }
+//        userService.saveBatch(userList, 1000);
+//        stopWatch.stop();
+//        System.out.println(stopWatch.getTotalTimeMillis());
+//    }
+//
+//    @Test
+//    public void doConcurrencyInsertUser() {
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//        final int INSERT_NUM = 10000;
+//
+//        int j = 0;
+//        int batchSize = 5000;
+//        List<CompletableFuture<Void>> futureList = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            List<User> userList = new ArrayList<>();
+//            while (true) {
+//                j++;
+//                User user = new User();
+//                user.setUsername("fake");
+//                user.setUserAccount("fakerara");
+//                user.setProfile("fake");
+//                user.setAvatarUrl("");
+//                user.setGender(0);
+//                user.setUserPassword("12345678");
+//                user.setPhone("123");
+//                user.setEmail("123@gmail.com");
+//                user.setUserStatus(0);
+//                user.setUserRole(0);
+//                user.setUserCode("111111");
+//                user.setTags("[]");
+//                userList.add(user);
+//                if (j % batchSize == 0) {
+//                    break;
+//                }
+//            }
+//            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+//                userService.saveBatch(userList, batchSize);
+//            }, executorService);
+//            futureList.add(future);
+//        }
+//        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[]{})).join();
+//
+//        stopWatch.stop();
+//        System.out.println(stopWatch.getTotalTimeMillis());
+//    }
 }
